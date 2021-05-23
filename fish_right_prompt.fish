bf7d550f486878e8f1db4789fcf8168b03b1e7c5
hoangpq <pquochoang2007@gmail.com>
2021-05-23 17:00:56 +0700

    wfh

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -6,6 +6,6 @@ function fish_right_prompt
 
   echo -n -s $color_dim "Rust: v"(rustc --version | awk '{print $2}')$color_off
   echo ", "
-  echo -n -s $color_dim "Node: "(nvm current)$color_off
+  echo -n -s $color_dim "⬢ "(nvm current)$color_off
   # echo -n -s $color_dim (date +%H$color_green:$color_dim%M$color_green:$color_dim%S)$color_off
 end
